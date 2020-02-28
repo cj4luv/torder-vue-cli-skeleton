e8922c4c678625f58f615c04e60589a3c31d006b
laon <cj4luv@gmail.com>
2020-02-28 17:34:45 +0900

    디렉토리 구분

---
+++ aliases.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const aliases = {
+  '@': '.',
+  '@src': 'src',
+  '@router': 'src/router',
+  '@views': 'src/router/views',
+  '@layouts': 'src/router/layouts',
+  '@components': 'src/components',
+  '@assets': 'src/assets',
+  '@utils': 'src/utils',
+  '@store': 'src/store',
+  '@sass': 'src/sass',
+};
+
+module.exports = {
+  webpack: {},
+};
+
+for (const alias in aliases) {
+  const aliasTo = aliases[alias];
+  module.exports.webpack[alias] = resolveSrc(aliasTo);
+}
+
+function resolveSrc(_path) {
+  return path.resolve(__dirname, _path);
+}
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+/** @type import('@vue/cli-service').ProjectOptions */
 module.exports = {
+  // https://github.com/neutrinojs/webpack-chain/tree/v4#getting-started
+  chainWebpack(config) {
+    // Set up all the aliases we use in our app.
+    config.resolve.alias.clear().merge(require('./aliases.config').webpack);
+  },
   // Configure Webpack's dev server.
   // https://cli.vuejs.org/guide/cli-service.html
   devServer: {
