98bc045bf56cd741fcf821632596072428c6e84c
laon <cj4luv@gmail.com>
2020-02-28 18:06:49 +0900

    라우터 추가

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import VueCookies from 'vue-cookies';
+
+import router from '@router';
+
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueAxios, axios);
+Vue.use(VueCookies);
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app');
---
+++ src/router/routes.js
@@ -0,0 +1,11 @@
+import { HelloWorld } from '@views';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HelloWorld,
+  },
+];
+
+export default routes;
